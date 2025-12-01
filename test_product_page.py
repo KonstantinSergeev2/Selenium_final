0c4cdcd93c365769c06272d80f24ce4fcf56ab87
KonstantinSergeev2 <kostyasergeev28821@gmail.com>
2025-12-02 01:15:47 +0500

    Сгруппировал тесты в классы

--- test_product_page.py
+++ test_product_page.py
@@ -1,9 +1,10 @@
 import pytest
 from pages.product_page import ProductPage
 from pages.login_page import LoginPage
+from pages.basket_page import BasketPage
 
 
-# Существующие тесты
+# Тесты для добавления товара в корзину
 @pytest.mark.parametrize('link', [
     "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear",
     "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
@@ -15,23 +16,26 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.should_be_product_added_to_basket()
 
 
-# НОВЫЕ ТЕСТЫ из задания
-def test_guest_should_see_login_link_on_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    page = ProductPage(browser, link)
-    page.open()
-    page.should_be_login_link()
+# Класс для тестов логина со страницы товара
+class TestLoginFromProductPage():
+    @pytest.mark.login_guest
+    def test_guest_should_see_login_link_on_product_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_be_login_link()
+
+    @pytest.mark.login_guest
+    def test_guest_can_go_to_login_page_from_product_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+        page = ProductPage(browser, link)
+        page.open()
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
 
 
-def test_guest_can_go_to_login_page_from_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-    page = ProductPage(browser, link)
-    page.open()
-    page.go_to_login_page()
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_page()
-
-
+# Тест для проверки корзины со страницы товара
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     """
     Гость открывает страницу товара
