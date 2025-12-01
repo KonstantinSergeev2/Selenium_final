3abab3e6913e43acaf41904d8ca454119ace9dca
KonstantinSergeev2 <kostyasergeev28821@gmail.com>
2025-12-02 00:18:47 +0500

    Добавлена LoginPage с проверками: URL, формы логина и регистрации

--- test_main_page.py
+++ test_main_page.py
@@ -5,7 +5,12 @@ def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
-    page.go_to_login_page()
+
+    # Переходим на страницу логина и получаем объект LoginPage
+    login_page = page.go_to_login_page()
+
+    # Проверяем, что это действительно страница логина
+    login_page.should_be_login_page()
 
 
 def test_guest_should_see_login_link(browser):
